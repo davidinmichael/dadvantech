653ec1199fda469b469f5febb621b9a5c6d58853
davidinmichael <davidinmichael@gmail.com>
2025-06-03 15:59:49 +0100

    Mod: Deployment settings.

--- dadvantech/wsgi.py
+++ dadvantech/wsgi.py
@@ -9,3 +9,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv("DJANGO_SETTINGS_MODULE"))
 
 application = get_wsgi_application()
+app = application
