8bf90a5152e1d0347c196c2b32d6b0e9a3801a25
davidinmichael <davidinmichael@gmail.com>
2025-06-03 12:36:11 +0100

    Mod: Added actual services and products.

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name="index"),
+	path('solar-products', views.products_in_drive, name="products_in_drive"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views import View
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def index(request):
     return render(request, "core/index.html")
+
+def products_in_drive(request):
+    url = os.getenv("PRODUCTS_IN_DRIVE_LINK")
+    return redirect(url)
