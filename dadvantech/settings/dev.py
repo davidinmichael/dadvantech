2bd78cc36430107cc8176af5104c905cc33891e5
davidinmichael <davidinmichael@gmail.com>
2025-06-03 16:46:17 +0100

    Mod: Prod

---
+++ dadvantech/settings/dev.py
@@ -0,0 +1,19 @@
+from .base import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS").split(",")
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
